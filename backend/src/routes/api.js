f673fddbf097764851d0616b67a4d44ccf00ddc1
SilentCodeSage <nandunandakishor345@gmail.com>
2024-10-25 20:19:28 +0530

    evaluate excuse api implemented

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -27,4 +27,29 @@ apiRouter.post("/api/generate", async (req, res) => {
   }
 });
 
+// Evaluate the excuse input by a user and then rate it
+apiRouter.post("/api/evaluate", async (req, res) => {
+  try {
+    const userPrompt = req.body.prompt + ".Out of ten 10 rate this excuse based on how beleivable it is. only give a numbeer nothing else as answer";
+    const apiKey = "AIzaSyARygGMBdAi0o4o6fAWjcayONjunxgU_mU";
+    const response = await axios.post(
+      `${GeminiContentGenerator}=${apiKey}`,
+      {
+        contents: [{ parts: [{ text: userPrompt }] }],
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    res.json(response.data);
+    console.log(response.data.candidates[0].content.parts[0].text);
+  } catch (error) {
+    console.error("Error fetching AI response:", error);
+    res.status(500).send("Error generating response");
+  }
+});
+
 module.exports = apiRouter;
